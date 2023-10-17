f3d1ad5e4a0a6f06bfa419f289a3b8cf138a34a8
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-17 11:31:16 +0200

    Solution to the problem n°4

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Problem 4.
+# Median of Two Sorted Arrays
+from typing import List
 
 
-# Press the green button in the gutter to run the script.
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        nums3 = self.mergeTwoSortedArrays(nums1, nums2)
+        index = int(len(nums3) / 2)
+        if len(nums3) % 2:
+            # Even
+            return nums3[index]
+        else:
+            # Odd
+            return (nums3[index - 1] + nums3[index]) / 2
+
+    def mergeTwoSortedArrays(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        i = 0  # iterator for nums1
+        j = 0  # iterator for nums2
+        k = 0  # iterator for nums3
+        lenNums1 = len(nums1)
+        lenNums2 = len(nums2)
+        nums3 = [None] * (lenNums1 + lenNums2)
+
+        # traverse both arrays
+        while i < lenNums1 and j < lenNums2:
+            if nums1[i] < nums2[j]:
+                nums3[k] = nums1[i]
+                i = i + 1
+            else:
+                nums3[k] = nums2[j]
+                j = j + 1
+            k = k + 1
+        # Store remaining elements
+        while i < lenNums1:
+            nums3[k] = nums1[i]
+            i = i + 1
+            k = k + 1
+        while j < lenNums2:
+            nums3[k] = nums2[j]
+            j = j + 1
+            k = k + 1
+
+        return nums3
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    nums1 = [1, 3]
+    nums2 = [2]
+    print("Nums1: " + str(nums1) + " Nums2: " + str(nums2) + " Median: " +
+          str(Solution().findMedianSortedArrays(nums1, nums2)))
