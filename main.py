f739efa7e38083c1cbbf020f474f03c6d8fb15d0
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-17 15:45:55 +0200

    Solution to the problem n°1361

--- main.py
+++ main.py
@@ -1,16 +1,59 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Problem 1361.
+# Validate Binary Tree Nodes.
+from typing import List
 
 
-# Press the green button in the gutter to run the script.
+class Solution:
+    def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
+        # 1) Check if every child has a unique parent
+        parents = [-1] * n
+        for i in range(n):
+            if leftChild[i] != -1:
+                if parents[leftChild[i]] == -1:
+                    parents[leftChild[i]] = i
+                else:
+                    return False
+            if rightChild[i] != -1:
+                if parents[rightChild[i]] == -1:
+                    parents[rightChild[i]] = i
+                else:
+                    return False
+        # 2) Find the root
+        root = -1
+        for i in range(n):
+            if parents[i] == -1:
+                # There is only one root
+                if root == -1:
+                    root = i
+                else:
+                    return False
+        # The root must exist
+        if root == -1:
+            return False
+        # 3) Detect cycles
+        visited = []
+        toVisit = []
+        toVisit.insert(0, root)
+        while len(toVisit) != 0:
+            node = toVisit.pop()
+            if node in visited:
+                return False
+            visited.insert(0, node)
+            if leftChild[node] != -1:
+                toVisit.append(leftChild[node])
+            if rightChild[node] != -1:
+                toVisit.append(rightChild[node])
+        return len(visited) == n
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    n = 4
+    leftChild = [1, -1, 3, -1]
+    rightChild = [2, -1, -1, -1]
+    print("Input: n = " + str(n) + " leftChild = " + str(leftChild) + " rightChild = " + str(rightChild))
+    print("Output: " + str(Solution().validateBinaryTreeNodes(n, leftChild, rightChild)))
+    n = 6
+    leftChild = [1, -1, -1, 4, -1, -1]
+    rightChild = [2, -1, -1, 5, -1, -1]
+    print("Input: n = " + str(n) + " leftChild = " + str(leftChild) + " rightChild = " + str(rightChild))
+    print("Output: " + str(Solution().validateBinaryTreeNodes(n, leftChild, rightChild)))
