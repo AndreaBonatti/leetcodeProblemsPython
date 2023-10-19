2e9f4bd031b673b1ac4c08a31f30b32652e7e769
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-19 11:02:11 +0200

    Another solution to the problem n°844

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
 # Problem 844.
 # Backspacing String Compare.
+from typing import List
+
 
 class Solution:
-    def getProcessedString(self, s: str):
-        stack = []
-        for char in s:
-            if char == '#':
-                if len(stack) != 0:
-                    stack.pop()
+    # While this function calculates the effective length of the string
+    # it also moves all the characters at the start of the string
+    # to use to do a comparison later
+    # e.g. a#bc => acbc
+    def getEffectiveLength(self, s: List[str]) -> int:
+        length = 0
+        for i in range(len(s)):
+            if s[i] == '#':
+                if length != 0:
+                    length -= 1
             else:
-                stack.append(char)
-        return stack
+                s[length] = s[i]
+                length += 1
+        return length
 
     def backspaceCompare(self, s: str, t: str) -> bool:
-        return self.getProcessedString(s) == self.getProcessedString(t)
+        s = list(s)
+        t = list(t)
+        sLength = self.getEffectiveLength(s)
+        tLength = self.getEffectiveLength(t)
+        if sLength == tLength:
+            if s[0:sLength] != t[0:sLength]:
+                return False
+            return True
+        return False
 
 
 if __name__ == '__main__':
