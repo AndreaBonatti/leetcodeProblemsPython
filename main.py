4e6dbd7f622dcc0e926e6fb6b8f0887ede8eb53b
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-20 11:33:46 +0200

    Solution to the problem n°0005 with O(n^2) time complexity

--- main.py
+++ main.py
@@ -2,25 +2,26 @@
 # Longest Palindromic Substring.
 
 class Solution:
-    def isPalindorme(self, s: str) -> bool:
-        return s == s[::-1]
-
     def longestPalindrome(self, s: str) -> str:
-        if len(s) == 0:
-            return ""
-        length = 1
-        longestPalindrome = s[0]
-        allSubstrings = []
-        start = 0
-        while start < len(s) - 1:
-            for end in range(start + 1, len(s) + 1):
-                if s[start:end] not in allSubstrings:
-                    allSubstrings.append(s[start:end])
-                    if self.isPalindorme(s[start:end]) and length < len(s[start:end]):
-                        length = len(s[start:end])
-                        longestPalindrome = s[start:end]
-            start += 1
-        return longestPalindrome
+
+        # To find the longest palindrome from a starting point
+        def expand(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left + 1:right]
+
+        result = ""
+        for i in range(len(s)):
+            # The starting point is a single letter => odd string
+            sub1 = expand(i, i)
+            if len(sub1) > len(result):
+                result = sub1
+            # The starting point is a couple of letter => even string
+            sub2 = expand(i, i+1)
+            if len(sub2) > len(result):
+                result = sub2
+        return result
 
 
 if __name__ == '__main__':
