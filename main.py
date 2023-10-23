02aae9730c06da21adc22c8b0b643a17e53201ce
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-23 09:41:27 +0200

    Solution to the problem n°0006

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+# Problem 6.
+# Zigzag Conversion.
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [[] for _ in range(numRows)]
+        rowIndex = 0
+        # +1 => go down, -1 go up
+        step = 1
+        for char in s:
+            rows[rowIndex].append(char)
+            # Change step direction when we reach the top or bottom row
+            if rowIndex == 0:
+                step = 1
+            elif rowIndex == numRows - 1:
+                step = -1
+            rowIndex += step
+
+        for i in range(numRows):
+            rows[i] = "".join(rows[i])
+        return ''.join(rows)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    numRows = 3
+    string = "PAYPALISHIRING"
+    print("Input: s = \"" + string + "\" numRows = " + str(numRows) +
+          " - Output: " + str(Solution().convert(string, numRows)))
+    numRows = 4
+    string = "PAYPALISHIRING"
+    print("Input: s = \"" + string + "\" numRows = " + str(numRows) +
+          " - Output: " + str(Solution().convert(string, numRows)))
+    numRows = 1
+    string = "A"
+    print("Input: s = \"" + string + "\" numRows = " + str(numRows) +
+          " - Output: " + str(Solution().convert(string, numRows)))
