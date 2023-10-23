23f83373f83102d40bca7d836d89e8fc535e3872
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-23 12:01:41 +0200

    Solution to the problem n°32

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+# Problem 32.
+# Longest Valid Parentheses.
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        output = 0
+        stack = [-1]
+        for index, char in enumerate(s):
+            if char == "(":
+                stack.append(index)
+            else:
+                stack.pop()
+                if stack:
+                    output = max(output, index - stack[-1])
+                else:
+                    stack.append(index)
+
+        return output
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    inputs = ["()(()", "(()", ")()())", ""]
+    for string in inputs:
+        print("Input: s= \"" + string + "\"")
+        print("Output: " + str(Solution().longestValidParentheses(string)))
