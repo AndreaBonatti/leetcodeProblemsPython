e4c1f5375170c628d46020a8b2f997bc848073f7
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-24 09:46:21 +0200

    Solution to the problem n°222

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Problem 222.
+# Count Complete Tree Nodes.
+from collections import deque
+from typing import Optional
 
 
-# Press the green button in the gutter to run the script.
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        # empty tree
+        if not root:
+            return 0
+
+        queue = deque([root])
+        result = 0
+
+        # BFS
+        while queue:
+            level_nodes_number = len(queue)
+
+            for _ in range(level_nodes_number):
+                node = queue.popleft()
+                result += 1
+
+                # Add the children node to the queue (ignored in this for because we consider the level_nodes_number)
+                for child in [node.left, node.right]:
+                    if child:
+                        queue.append(child)
+
+        return result
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    root = TreeNode(
+        1,
+        TreeNode(
+            2,
+            TreeNode(4),
+            TreeNode(5)
+        ),
+        TreeNode(
+            3,
+            TreeNode(6),
+            None
+        )
+    )
+    print(str(Solution().countNodes(root)))
+    root = None
+    print(str(Solution().countNodes(root)))
+    root = TreeNode(1)
+    print(str(Solution().countNodes(root)))
