16e6b6a1570e89c63a0ada1558e7dbae53b88ad7
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-25 09:24:18 +0200

    Solution to the problem n°779

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+# Problem 779.
+# K-th Symbol in Grammar.
 
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if n == 1:
+            return 0
+        halfRowLength = 2 ** (n - 2)
+        if k > halfRowLength:
+            return 1 if self.kthGrammar(n - 1, k - halfRowLength) == 0 else 0
+        else:
+            return self.kthGrammar(n - 1, k)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    n = 1
+    k = 1
+    print("Input: n = " + str(n) + ", k = " + str(k))
+    print("Output: " + str(Solution().kthGrammar(n, k)))
+    n = 2
+    k = 1
+    print("Input: n = " + str(n) + ", k = " + str(k))
+    print("Output: " + str(Solution().kthGrammar(n, k)))
+    n = 2
+    k = 2
+    print("Input: n = " + str(n) + ", k = " + str(k))
+    print("Output: " + str(Solution().kthGrammar(n, k)))
