93654ea77650b38d4c9d28901ff631ea51d383f2
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-26 15:16:36 +0200

    Solution to the problem n°698 but it is very slow if there are a lot of numbers

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Problem 698.
+# Partition to K Equal Sum Subsets.
+from typing import List
 
 
-# Press the green button in the gutter to run the script.
+class Solution:
+    def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
+        total = sum(nums)
+        # If it is not possible divide the total in k parts without having a remainder
+        # then we cannot have the k partitions
+        if total % k != 0:
+            return False
+        subset_value = total // k
+        subsets = [0] * k
+        nums.sort(reverse=True)
+        # The biggest number must be at least equal to the subset value
+        if nums[0] > subset_value:
+            return False
+
+        def dfs(current_value: int, remaining_subsets: int, numbers: List[int]) -> bool:
+            if current_value > subset_value:
+                return False
+            # A set is found
+            if current_value == subset_value:
+                remaining_subsets -= 1
+                current_value = 0
+            # If there are no more numbers in the original array and k is 0 we found the solution
+            if not numbers:
+                return not remaining_subsets
+            # If there are more numbers we recursive call dfs
+            for i in range(len(numbers)):
+                if dfs(current_value + numbers[i], remaining_subsets, numbers[:i] + numbers[i + 1:]):
+                    return True
+
+        return dfs(0, k, nums)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    nums = [4, 3, 2, 3, 5, 2, 1]
+    k = 4
+    print("Input: nums= " + str(nums) + " k = " + str(k))
+    print("Output: " + str(Solution().canPartitionKSubsets(nums, k)))
+    nums = [1, 2, 3, 4]
+    k = 3
+    print("Input: nums= " + str(nums) + " k = " + str(k))
+    print("Output: " + str(Solution().canPartitionKSubsets(nums, k)))
+    nums = [3, 3, 10, 2, 6, 5, 10, 6, 8, 3, 2, 1, 6, 10, 7, 2]
+    k = 6
+    print("Input: nums= " + str(nums) + " k = " + str(k))
+    print("Output: " + str(Solution().canPartitionKSubsets(nums, k)))
