5c9097d07e16875fa932775a5a6afef1709c85e4
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-26 09:38:05 +0200

    Solution to the problem n°823 The solution comes from this fantastic video: https://www.youtube.com/watch?v=IAYKQbZoZRg

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Problem 823.
+# Binary Trees With Factors.
+from typing import List
 
 
-# Press the green button in the gutter to run the script.
+class Solution:
+    def numFactoredBinaryTrees(self, arr: List[int]) -> int:
+        arr.sort()
+        # The dictionary contains the number of binary tree starting from a specific node as root
+        # We start with one tree for every node that is made only by a single node
+        dct = {item: 1 for item in arr}
+        for root in arr:
+            for factor in arr:
+                # If we reach the same node we stop because the array is sorted
+                # if we continue we only find bigger nodes that could not be leaves for new tree
+                if factor == root:
+                    break
+                # If root is divisible by the selected factor
+                # and the result of division is the dictionary === is in the array
+                # Then we add the new trees in the array
+                if root % factor == 0 and root // factor in dct:
+                    # It works like this because we sort the array at the start
+                    dct[root] += dct[factor] * dct[root // factor]
+
+        return sum(dct.values()) % (pow(10, 9) + 7)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    arr = [2, 4]
+    print("Input: arr = " + str(arr))
+    print("Output: " + str(Solution().numFactoredBinaryTrees(arr)))
+    arr = [2, 4, 5, 10]
+    print("Input: arr = " + str(arr))
+    print("Output: " + str(Solution().numFactoredBinaryTrees(arr)))
