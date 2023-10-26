cfce076a75e5893225b0c5aef32e6a93553ca74b
Andrea Bonatti <andreabonatti92@gmail.com>
2023-10-26 15:52:56 +0200

    Solution to the problem n°698 --> https://leetcode.com/problems/partition-to-k-equal-sum-subsets/solutions/2281522/python-90ms-faster-than-92-95-less-memory/

--- main.py
+++ main.py
@@ -8,34 +8,34 @@ class Solution:
         total = sum(nums)
         # If it is not possible divide the total in k parts without having a remainder
         # then we cannot have the k partitions
-        if total % k != 0:
+        if total % k:
             return False
+
         subset_value = total // k
         subsets = [0] * k
         nums.sort(reverse=True)
-        # The biggest number must be at least equal to the subset value
-        if nums[0] > subset_value:
+
+        def dfs(curr: int) -> bool:
+            if curr == len(nums):
+                return True
+            for i in range(k):
+                if subsets[i] + nums[curr] <= subset_value:
+                    subsets[i] += nums[curr]
+                    if dfs(curr + 1):
+                        return True
+                    subsets[i] -= nums[curr]
+                    if subsets[i] == 0:
+                        break
             return False
 
-        def dfs(current_value: int, remaining_subsets: int, numbers: List[int]) -> bool:
-            if current_value > subset_value:
-                return False
-            # A set is found
-            if current_value == subset_value:
-                remaining_subsets -= 1
-                current_value = 0
-            # If there are no more numbers in the original array and k is 0 we found the solution
-            if not numbers:
-                return not remaining_subsets
-            # If there are more numbers we recursive call dfs
-            for i in range(len(numbers)):
-                if dfs(current_value + numbers[i], remaining_subsets, numbers[:i] + numbers[i + 1:]):
-                    return True
-
-        return dfs(0, k, nums)
+        return dfs(0)
 
 
 if __name__ == '__main__':
+    nums = [2, 2, 2, 2, 3, 4, 5]
+    k = 4
+    print("Input: nums= " + str(nums) + " k = " + str(k))
+    print("Output: " + str(Solution().canPartitionKSubsets(nums, k)))
     nums = [4, 3, 2, 3, 5, 2, 1]
     k = 4
     print("Input: nums= " + str(nums) + " k = " + str(k))
