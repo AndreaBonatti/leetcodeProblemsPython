3f3efb5cc71e7d908b81605b14aa7e217d3b33e4
Andrea Bonatti <andreabonatti92@gmail.com>
2023-11-07 09:13:13 +0100

    Solution to the problem n°1921

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Problem 1921.
+# Eliminate Maximum Number of Monsters.
+from typing import List
 
 
-# Press the green button in the gutter to run the script.
+class Solution:
+    def eliminateMaximum(self, dist: List[int], speed: List[int]) -> int:
+        # Ordering the monsters by the arrival time to the city
+        arrival_time = [dist[i] / speed[i] for i in range(len(dist))]
+        arrival_time.sort()
+        for i in range(len(arrival_time)):
+            if arrival_time[i] <= i:
+                return i
+        return len(dist)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    dist = [1, 3, 4]
+    speed = [1, 1, 1]
+    print(f"Input: dist = {dist}, speed = {speed}")
+    print(f"Output: {Solution().eliminateMaximum(dist, speed)}")
+    dist = [1, 1, 2, 3]
+    speed = [1, 1, 1, 1]
+    print(f"Input: dist = {dist}, speed = {speed}")
+    print(f"Output: {Solution().eliminateMaximum(dist, speed)}")
+    dist = [3, 2, 4]
+    speed = [5, 3, 2]
+    print(f"Input: dist = {dist}, speed = {speed}")
+    print(f"Output: {Solution().eliminateMaximum(dist, speed)}")
