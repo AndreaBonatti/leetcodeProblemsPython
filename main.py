669d105fadf4bdc0c6cc557f0993911642f01300
Andrea Bonatti <andreabonatti92@gmail.com>
2023-11-08 15:05:22 +0100

    Solution to the problem n°2849

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Maiusc+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Problem 2849.
+# Determine if a Cell is reachable at a Given Time.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Solution:
+    def isReachableAtTime(self, sx: int, sy: int, fx: int, fy: int, t: int) -> bool:
+        if 0 <= t <= 10 ** 9:
+            # If the 2 position are the same:
+            # with one move, we can only go away from target.
+            # Even for t == 0, it would be true as we would be at target without moving anywhere.
+            # For t > 1, we can always come back to the cell.
+            if sx == fx and sy == fy:
+                return t != 1
+            # Chebyshev_distance
+            return max(abs(sx - fx), abs(sy - fy)) <= t
+        else:
+            return False
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    sx = 1
+    sy = 2
+    fx = 1
+    fy = 2
+    t = 1
+    print(f"Input: sx = {sx}, sy = {sy}, fx = {fx}, fy = {fy}, t = {t}")
+    print(f"Output: {Solution().isReachableAtTime(sx, sy, fx, fy, t)}")
+    sx = 2
+    sy = 4
+    fx = 7
+    fy = 7
+    t = 6
+    print(f"Input: sx = {sx}, sy = {sy}, fx = {fx}, fy = {fy}, t = {t}")
+    print(f"Output: {Solution().isReachableAtTime(sx, sy, fx, fy, t)}")
+    sx = 3
+    sy = 1
+    fx = 7
+    fy = 3
+    t = 3
+    print(f"Input: sx = {sx}, sy = {sy}, fx = {fx}, fy = {fy}, t = {t}")
+    print(f"Output: {Solution().isReachableAtTime(sx, sy, fx, fy, t)}")
